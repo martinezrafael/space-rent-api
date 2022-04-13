bd06aa062b1bd300a3dca3bd7ff31b763fb00138
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2022-04-12 21:41:39 -0300

    criado o modelo de Space

---
+++ models/Space.model.js
@@ -0,0 +1,23 @@
+const { model, Schema } = require("mongoose");
+
+const spaceSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        adress: {
+            street: String,
+            city: String,
+            zipcode: String,
+        },
+        size: String,
+        purposes: Array,
+        user: { type: Schema.Types.ObjectId, ref: "User" },
+        comments: { type: Schema.Types.ObjectId, ref: "Comments" }
+
+    }
+)
+
+module.exports = model('Space', spaceSchema);
