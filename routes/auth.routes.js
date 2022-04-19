dd79973455aa2042b355b52877c38d00c741a451
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2022-04-19 18:53:46 -0300

    criada a rota de login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,8 @@ const router = Router();
 
 const bcrypt = require('bcryptjs');
 
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/User.model');
 
 router.post('/signup', async (req, res) => {
@@ -31,7 +33,40 @@ router.post('/signup', async (req, res) => {
 
         res.status(201).json('User created!')
     } catch (error) {
-        res.status(500).json({message: 'Error creating user', error})
+        res.status(500).json({message: 'Error creating user', error: error.message})
+    }
+})
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        if(!username || !password){
+            throw new Error('Preencha as informações para acessar');
+        }
+
+        const userFromDb = await User.findOne({username})
+        if(!userFromDb){
+            throw new Error('Nome de usuário ou senha incorretos');
+        }
+
+        const validation = bcrypt.compareSync(password, userFromDb.passwordHash);
+        if(!validation) {
+            throw new Error('Nome de usuário ou senha incorretos')
+        }
+        
+        const payload = {
+            id: userFromDb._id,
+            username: userFromDb.username
+        };
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: '1day'
+        });
+
+        res.status(200).json({user: payload, token});
+
+    } catch (error) {
+        res.status(500).json({message: 'Error trying to login', error: error.message});
     }
 })
 
